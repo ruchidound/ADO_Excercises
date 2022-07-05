03195529ccb6e54f83596d87ef285c23fc76fd29
ruchidound <rupalidound02@gmail.com>
2022-07-05 16:32:30 +0530

    Add project files.

---
+++ ADO_Excercises/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace ADO_Excercises
+{
+    partial class FrmConnected
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudentCode = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.TxtStudentCode = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtDCode = new System.Windows.Forms.TextBox();
+            this.TxtDOB = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnInsert = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStudentCode
+            // 
+            this.lblStudentCode.AutoSize = true;
+            this.lblStudentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentCode.Location = new System.Drawing.Point(108, 200);
+            this.lblStudentCode.Name = "lblStudentCode";
+            this.lblStudentCode.Size = new System.Drawing.Size(145, 26);
+            this.lblStudentCode.TabIndex = 0;
+            this.lblStudentCode.Text = "Student Code";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(108, 261);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(152, 26);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Student Name";
+            // 
+            // lblDept
+            // 
+            this.lblDept.AutoSize = true;
+            this.lblDept.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDept.Location = new System.Drawing.Point(106, 322);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(184, 26);
+            this.lblDept.TabIndex = 2;
+            this.lblDept.Text = "Department Code";
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDOB.Location = new System.Drawing.Point(106, 381);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(141, 26);
+            this.lblDOB.TabIndex = 3;
+            this.lblDOB.Text = "Student DOB";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(108, 443);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(92, 26);
+            this.lblAddress.TabIndex = 4;
+            this.lblAddress.Text = "Address";
+            // 
+            // TxtStudentCode
+            // 
+            this.TxtStudentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtStudentCode.Location = new System.Drawing.Point(296, 194);
+            this.TxtStudentCode.Name = "TxtStudentCode";
+            this.TxtStudentCode.Size = new System.Drawing.Size(205, 32);
+            this.TxtStudentCode.TabIndex = 5;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtName.Location = new System.Drawing.Point(296, 255);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(205, 32);
+            this.TxtName.TabIndex = 6;
+            // 
+            // TxtDCode
+            // 
+            this.TxtDCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDCode.Location = new System.Drawing.Point(296, 316);
+            this.TxtDCode.Name = "TxtDCode";
+            this.TxtDCode.Size = new System.Drawing.Size(205, 32);
+            this.TxtDCode.TabIndex = 7;
+            // 
+            // TxtDOB
+            // 
+            this.TxtDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDOB.Location = new System.Drawing.Point(296, 375);
+            this.TxtDOB.Name = "TxtDOB";
+            this.TxtDOB.Size = new System.Drawing.Size(205, 32);
+            this.TxtDOB.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(296, 445);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(205, 32);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSearch.Location = new System.Drawing.Point(565, 184);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(209, 42);
+            this.BtnSearch.TabIndex = 10;
+            this.BtnSearch.Text = "Search";
+            this.BtnSearch.UseVisualStyleBackColor = false;
+            this.BtnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(565, 245);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(209, 42);
+            this.BtnReset.TabIndex = 11;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // BtnInsert
+            // 
+            this.BtnInsert.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInsert.Location = new System.Drawing.Point(565, 305);
+            this.BtnInsert.Name = "BtnInsert";
+            this.BtnInsert.Size = new System.Drawing.Size(209, 43);
+            this.BtnInsert.TabIndex = 12;
+            this.BtnInsert.Text = "Insert";
+            this.BtnInsert.UseVisualStyleBackColor = false;
+            this.BtnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdate.Location = new System.Drawing.Point(565, 365);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(209, 42);
+            this.BtnUpdate.TabIndex = 13;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.Location = new System.Drawing.Point(565, 435);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(209, 42);
+            this.BtnDelete.TabIndex = 14;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ADO_Excercises.Properties.Resources.Effective_method_to_Choose_Employee_Performance_Software_That_Eases_HR_Pain_Points_ss_blog;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(896, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmConnected
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 530);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnInsert);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.TxtDOB);
+            this.Controls.Add(this.TxtDCode);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtStudentCode);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.lblDept);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblStudentCode);
+            this.Name = "FrmConnected";
+            this.Text = "FrmConnected";
+            this.Load += new System.EventHandler(this.FrmConnected_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudentCode;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDept;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox TxtStudentCode;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtDCode;
+        private System.Windows.Forms.TextBox TxtDOB;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnInsert;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ADO_Excercises/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADO_Excercises
+{
+    public partial class FrmConnected : Form
+    {
+        public FrmConnected()
+        {
+            InitializeComponent();
+        }
+        private SqlConnection con = null;
+        private SqlCommand cmd = null;
+        private SqlDataReader reader = null;
+
+        private void FrmConnected_Load(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("Select Stud_Code,Stud_Name,Dept_Code,Stud_Dob,Address from Student_master", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    using (reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            TxtStudentCode.Text = reader["Stud_Code"].ToString();
+                            TxtName.Text = reader["Stud_Name"].ToString();
+                            TxtDCode.Text = reader["Dept_Code"].ToString();
+                            TxtDOB.Text = reader["Stud_Dob"].ToString();
+                            txtAddress.Text = reader["Address"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Record");
+
+                        }
+                    }
+                }
+            }
+        }
+        //Search Student by StudentCode
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("Select Stud_Code,Stud_Name,Dept_Code,Stud_Dob,Address from Student_master where Stud_Code=@Student_Code ", con))
+                {
+                    cmd.Parameters.AddWithValue("@Student_Code", TxtStudentCode.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    using (reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            TxtName.Text = reader["Stud_Name"].ToString();
+                            TxtDCode.Text = reader["Dept_Code"].ToString();
+                            TxtDOB.Text = reader["Stud_Dob"].ToString();
+                            txtAddress.Text = reader["Address"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Record Found");
+                            this.ClearTest();
+                        }
+
+                    }
+                }
+            }
+        }
+        //Reset Form
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.ClearTest();
+        }
+        public void ClearTest()
+        {
+            TxtStudentCode.Text = "";
+            TxtName.Text = "";
+            TxtDCode.Text = "";
+            TxtDOB.Text = "";
+            txtAddress.Text = "";
+            TxtStudentCode.Focus();
+        }
+
+        //Insert the Records
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("usp_InsertStudentDetails", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Stud_Code", TxtStudentCode.Text);
+                    cmd.Parameters.AddWithValue("@Stud_Name", TxtName.Text);
+                    cmd.Parameters.AddWithValue("@Dept_Code", TxtDCode.Text);
+                    cmd.Parameters.AddWithValue("@Stud_Dob", TxtDOB.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("New Student Record Created!!");
+        }
+
+        //Update the Records
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("usp_UpdateDCodeAndAddressBySCode", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Dept_Code", TxtDCode.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                    cmd.Parameters.AddWithValue("@Stud_Code", TxtStudentCode.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("New Student Records Updated!!");
+        }
+
+        //Delete the Records
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["TrainingConnection"].ConnectionString))
+            {
+                using (cmd = new SqlCommand("Delete from Student_master where Stud_Code=@Stud_Code", con))
+                {
+                    cmd.Parameters.AddWithValue("@Stud_Code", TxtStudentCode.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("New Student Records Has Been Deleted!!");
+        }
+    }
+}
